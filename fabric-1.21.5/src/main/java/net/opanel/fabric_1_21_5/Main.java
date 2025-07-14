dbe7506199ced957947152fe038653a9a39a2473
NriotHrreion <nriot233@outlook.com>
2025-07-14 12:32:02 +0800

    fix: Change fabric module to dedicated server mod

--- fabric-1.21.5/src/main/java/net/opanel/fabric_1_21_5/Main.java
+++ fabric-1.21.5/src/main/java/net/opanel/fabric_1_21_5/Main.java
@@ -1,5 +1,6 @@
 package net.opanel.fabric_1_21_5;
 
+import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
@@ -10,13 +11,13 @@ import space.nocp.configx.api.*;
 
 import java.io.IOException;
 
-public class Main implements ModInitializer {
+public class Main implements DedicatedServerModInitializer {
     public static final String MODID = "opanel";
     public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
     public OPanel instance;
 
     @Override
-    public void onInitialize() {
+    public void onInitializeServer() {
         Configuration<OPanelConfiguration> config = ConfigManager.get().register(MODID, OPanelConfiguration.defaultConfig, OPanelConfiguration.class);
         instance = new OPanel(config.get(), new LoggerImpl(LOGGER));
 
