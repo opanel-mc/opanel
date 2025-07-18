7abc9b4c367f645554263cc9d6d86d45d3a51116
NriotHrreion <nriot233@outlook.com>
2025-07-18 12:11:48 +0800

    feat: Use badge to indicate server status in info card

--- frontend/app/panel/dashboard/info-card.tsx
+++ frontend/app/panel/dashboard/info-card.tsx
@@ -3,14 +3,10 @@
 import { useContext } from "react";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger
-} from "@/components/ui/tooltip";
 import { apiUrl } from "@/lib/api";
 import { InfoContext } from "@/contexts/api-context";
 import { MinecraftText } from "@/components/mc-text";
+import { Badge } from "@/components/ui/badge";
 
 import PackIcon from "@/assets/images/pack.png";
 
@@ -28,7 +24,7 @@ export function InfoCard({
         src={(ctx && ctx.favicon) ? (apiUrl + ctx.favicon) : PackIcon.src}
         alt="favicon"/>
       
-      <div className="flex-1 flex flex-col gap-1">
+      <div className="flex-1 flex flex-col gap-2">
         <div className="flex justify-between items-center *:text-nowrap">
           <div className="flex gap-4 [&>*]:space-x-2">
             <div>
@@ -44,18 +40,14 @@ export function InfoCard({
               <span className="text-emerald-500 font-[Consolas]">{ctx ? ctx.port : ""}</span>
             </div>
           </div>
-          <div className="pr-2">
-            <Tooltip>
-              <TooltipTrigger>
-                <div className={cn("w-2 h-2 rounded-full", ctx ? "bg-green-600" : "bg-red-700")}/>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>{ctx ? "正在运行" : "未运行"}</p>
-              </TooltipContent>
-            </Tooltip>
+          <div className="ml-2">
+            <Badge variant="outline">
+              <div className={cn("w-2 h-2 rounded-full", ctx ? "bg-green-600" : "bg-red-700")}/>
+              {ctx ? "正在运行" : "未运行"}
+            </Badge>
           </div>
         </div>
-        <div className="flex-1">
+        <div className="flex-1 text-sm">
           {ctx && <MinecraftText>{"§7"+ atob(ctx.motd)}</MinecraftText>}
         </div>
       </div>
