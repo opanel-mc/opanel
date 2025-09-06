c66f8ba91e6f919ccad3843a306d3fdad4fd89c0
NriotHrreion <nriot233@outlook.com>
2025-09-06 09:56:34 +0800

    feat(fabric): Add support for 1.21.3 and 1.21.4

--- README.md
+++ README.md
@@ -36,7 +36,7 @@ The following target platforms and minecraft versions are supported:
 |Server Type|Version|
 |---|---|
 |Bukkit / Spigot / Paper|>=1.21|
-|Fabric|>=1.21.5|
+|Fabric|>=1.21.3|
 |Forge|1.21-1.21.1, >=1.21.3|
 |Neoforge|>=1.21.5|
 
---
+++ fabric-1.21.3/src/main/java/net/opanel/fabric_1_21_3/LoggerImpl.java
@@ -0,0 +1,78 @@
+package net.opanel.fabric_1_21_3;
+
+import net.opanel.logger.Loggable;
+import net.opanel.utils.Utils;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LoggerImpl implements Loggable {
+    private static final Path logFolderPath = Paths.get("").resolve("logs");
+
+    private final Logger logger;
+
+    LoggerImpl(Logger logger) {
+        this.logger = logger;
+    }
+
+    @Override
+    public void info(String msg) {
+        logger.info(msg);
+    }
+
+    @Override
+    public void warn(String msg) {
+        logger.warn(msg);
+    }
+
+    @Override
+    public void error(String msg) {
+        logger.error(msg);
+    }
+
+    @Override
+    public List<String> getLogFileList() throws IOException {
+        if(!Files.exists(logFolderPath)) {
+            Files.createDirectory(logFolderPath);
+        }
+        if(!Files.isDirectory(logFolderPath)) {
+            throw new IOException("Cannot find the logs folder, but found a logs file.");
+        }
+
+        List<String> fileList = new ArrayList<>();
+        try(Stream<Path> stream = Files.list(logFolderPath)) {
+            stream.filter(item -> !Files.isDirectory(item))
+                    .map(Path::getFileName)
+                    .forEach(name -> fileList.add(name.toString()));
+        }
+        return fileList;
+    }
+
+    @Override
+    public String getLogContent(String fileName) throws IOException {
+        final Path filePath = Paths.get(logFolderPath.toString(), fileName);
+        if(!Files.exists(filePath)) {
+            throw new IOException("Cannot find the specified log file.");
+        }
+        if(filePath.toString().endsWith(".log") || filePath.toString().endsWith("txt")) {
+            return Utils.readTextFile(filePath);
+        }
+        if(filePath.toString().endsWith(".gz")) {
+            return Utils.decompressTextGzip(filePath);
+        }
+        throw new IOException("Unexpected file extension.");
+    }
+
+    @Override
+    public void deleteLog(String fileName) throws IOException {
+        final Path filePath = Paths.get(logFolderPath.toString(), fileName);
+        if(!Files.exists(filePath)) {
+            throw new IOException("Cannot find the specified log file.");
+        }
+        Files.delete(filePath);
+    }
+}
