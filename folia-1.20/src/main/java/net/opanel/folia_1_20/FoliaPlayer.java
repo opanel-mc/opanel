7ec9422be38f3fa4a97e102b93aa516f44093146
HUGO_CHINA <92572306+HUGO123-yg@users.noreply.github.com>
2025-09-11 22:22:43 +0800

    feat(folia): 添加对Folia 1.20的支持
    添加Folia 1.20模块，包括核心功能实现、配置文件管理、命令处理、日志监听等功能
    实现玩家管理、白名单、存档管理等接口适配
    新增Folia特有的线程调度处理

---
+++ folia-1.20/src/main/java/net/opanel/folia_1_20/FoliaPlayer.java
@@ -0,0 +1,106 @@
+package net.opanel.folia_1_20;
+
+import net.opanel.common.OPanelGameMode;
+import net.opanel.common.OPanelPlayer;
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+
+import java.util.Date;
+
+public class FoliaPlayer implements OPanelPlayer {
+    private final Main plugin;
+    private final Player player;
+
+    public FoliaPlayer(Main plugin, Player player) {
+        this.plugin = plugin;
+        this.player = player;
+    }
+
+    @Override
+    public String getName() {
+        return player.getName();
+    }
+
+    @Override
+    public String getUUID() {
+        return player.getUniqueId().toString();
+    }
+
+    @Override
+    public boolean isOnline() {
+        return true;
+    }
+
+    @Override
+    public boolean isOp() {
+        return player.isOp();
+    }
+
+    @Override
+    public boolean isBanned() {
+        return false;
+    }
+
+    @Override
+    public OPanelGameMode getGameMode() {
+        GameMode gamemode = player.getGameMode();
+        switch(gamemode) {
+            case ADVENTURE -> { return OPanelGameMode.ADVENTURE; }
+            case SURVIVAL -> { return OPanelGameMode.SURVIVAL; }
+            case CREATIVE -> { return OPanelGameMode.CREATIVE; }
+            case SPECTATOR -> { return OPanelGameMode.SPECTATOR; }
+        }
+        return null;
+    }
+
+    @Override
+    public void setGameMode(OPanelGameMode gamemode) {
+        plugin.runTask(() -> {
+            switch(gamemode) {
+                case ADVENTURE -> player.setGameMode(GameMode.ADVENTURE);
+                case SURVIVAL -> player.setGameMode(GameMode.SURVIVAL);
+                case CREATIVE -> player.setGameMode(GameMode.CREATIVE);
+                case SPECTATOR -> player.setGameMode(GameMode.SPECTATOR);
+            }
+        });
+    }
+
+    @Override
+    public void giveOp() {
+        if(isOp()) return;
+        plugin.runTask(() -> player.setOp(true));
+    }
+
+    @Override
+    public void depriveOp() {
+        if(!isOp()) return;
+        plugin.runTask(() -> player.setOp(false));
+    }
+
+    @Override
+    public void kick(String reason) {
+        plugin.runTask(() -> player.kickPlayer(reason));
+    }
+
+    @Override
+    public void ban(String reason) {
+        if(isBanned()) return;
+        plugin.runTask(() -> {
+            player.getServer().getBanList(BanList.Type.NAME).addBan(player.getName(), reason, null, null);
+            player.kickPlayer(reason);
+        });
+    }
+
+    @Override
+    public String getBanReason() {
+        return null;
+    }
+
+    @Override
+    public void pardon() { }
+
+    @Override
+    public int getPing() {
+        return player.getPing();
+    }
+}
